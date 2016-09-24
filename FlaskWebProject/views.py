6ee5dde5a03b328be5250c62b3b01304294a105f
Shah, Rohil R <rohil@gatech.edu>
2016-09-24 10:11:35 -0400

    DB create method now returning something relevant

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -11,13 +11,13 @@ from sqlalchemy_utils import database_exists, create_database
 @app.route('/create')
 def create():
     DB_CONFIG_DICT = {
-        'user': 'postgres',
-        'password': 'poptartps1234',
-        'host': 'localhost',
+        'user': 'renDEVous',
+        'password': 'qwerty232',
+        'host': 'http://rendezvousserver.azurewebsites.net',
         'port': 5432,
     }
     DB_CONN_FORMAT = "postgresql://{user}:{password}@{host}:{port}/{database}"
-    DB_CONN_URI_DEFAULT = (DB_CONN_FORMAT.format(database='postgres',**DB_CONFIG_DICT))
+    DB_CONN_URI_DEFAULT = (DB_CONN_FORMAT.format(database='rendezvousDB',**DB_CONFIG_DICT))
     engine = create_engine(DB_CONN_URI_DEFAULT)
     if not database_exists(engine.url):
         create_database(engine.url)
