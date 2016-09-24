b71742f37ab6aeec0f35d785a1fe5caa95226020
Shah, Rohil R <rohil@gatech.edu>
2016-09-24 13:16:50 -0400

    fixed authenticate. testing engine url call

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -13,15 +13,18 @@ Session = sessionmaker(bind=engine)
 
 @app.route('/create')
 def create():
-    createDB()
-    return "True"
+    return createDB()
 
 #returns api key based on hash of current datetime
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-    return json.dumps(md5_crypt.encrypt(datetime.utcnow().strftime('%m/%d/%Y')))
+@app.route('/authenticate', methods=['GET'])
+def authenticate(username, password):
+    session = Session()
+    s = session.query(User).get(username)
+    if s.password == password:
+        return json.dumps(md5_crypt.encrypt(datetime.utcnow().strftime('%m/%d/%Y')))
+    return json.dumps('false')
 
-@app.route('/addDevice', methods=['POST'])
+@app.route('/addDevice', methods=['GET'])
 def addDevice(username, newMacID):
     session = Session()
     s = session.query(User).get(username)
@@ -34,7 +37,7 @@ def addDevice(username, newMacID):
 #username is the name of current user
 #friend is gonna be a User object with all its attribuets (columns)
 #return json of the friends database belonging to this current user.
-@app.route('/addFriend', methods=['POST'])
+@app.route('/addFriend', methods=['GET'])
 def addFriend(username, friendname):
     session = Session()
     s = session.query(User).get(username)
@@ -52,7 +55,7 @@ def addFriend(username, friendname):
 #username1 is name of guy who initiates friend request
 #username 2 is name of person receiving friend request
 #message is string, which is the message user1 sends to user 2
-@app.route('/addReminder', methods=['POST'])
+@app.route('/addReminder', methods=['GET'])
 def addReminder(username1, username2, message):
     session = Session()
     s1 = session.query(User).get(username1)
