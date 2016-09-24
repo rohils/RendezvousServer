867d7579d95c0232e698af954be16a90546bf7aa
Shah, Rohil R <rohil@gatech.edu>
2016-09-24 10:07:08 -0400

    DB connection working

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,5 +5,21 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 
 @app.route('/create')
+def create():
+    DB_CONFIG_DICT = {
+        'user': 'postgres',
+        'password': 'poptartps1234',
+        'host': 'localhost',
+        'port': 5432,
+    }
+    DB_CONN_FORMAT = "postgresql://{user}:{password}@{host}:{port}/{database}"
+    DB_CONN_URI_DEFAULT = (DB_CONN_FORMAT.format(database='postgres',**DB_CONFIG_DICT))
+    engine = create_engine(DB_CONN_URI_DEFAULT)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    print(database_exists(engine.url))
+    return DB_CONN_FORMAT
