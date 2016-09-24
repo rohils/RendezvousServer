adfa83220ea5c3fbe2be86cf4d03968108bb4a85
zhangj150 <zhangj@gatech.edu>
2016-09-24 11:03:21 -0400

    Handling merge conflicts

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template, request
 from FlaskWebProject import app
+import hashlib
 
 @app.route('/create')
 
@@ -13,6 +14,12 @@ from FlaskWebProject import app
 def authenticate(id):
     s = Users.query.get(id)
     toHash = s.password._convert(s.username)
+
+    d = datetime.utcnow()
+    d = d.strftime('%m/%d/%Y')
+    m = hashlib.md5()
+    m.update(d)
+    
     #cirque du twerque of all api key generations
     return json.dumps(toHash)
 
@@ -23,7 +30,12 @@ def addDevice(username, newMacID):
     apiKey = s.apiKey
 
     if apiKey == s.password._convert(s.username):
-        mac.append(newMacID)
+        s.macids = s.macids.append
         db.sessions.commit()
         return true
     else: return false
+
+"""def addFriend(username):
+    s = Users.query.get(username)
+    #friends = s.
+    #i think we need a friends class in init to hold another table."""
