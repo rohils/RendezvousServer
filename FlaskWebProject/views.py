887493c984c5ee776ccc0d7352c9fc293d42abd8
zhangj150 <zhangj@gatech.edu>
2016-09-24 12:49:36 -0400

    changed addFriend, addReminder

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -15,7 +15,7 @@ def create():
 
 #returns api key based on hash of current datetime
 @app.route('/authenticate', methods=['POST'])
-def authenticate(id):
+def authenticate():
     return json.dumps(md5_crypt.encrypt(datetime.utcnow().strftime('%m/%d/%Y')))
 
 @app.route('/addDevice', methods=['POST'])
@@ -34,12 +34,27 @@ def addDevice(username, newMacID):
 #friend is gonna be a User object with all its attribuets (columns)
 #return json of the friends database belonging to this current user.
 @app.route('/addFriend', methods=['POST'])
-def addFriend(username, friend):
-    s = Users.query.get(username)
+def addFriend(username, friendname):
+    s = session.query(User).get(username)
+    f = session.query(User).get(friendname)
     #dont know if below line works like this.
-    s.friends.append(friend)
-    db.sessions.commit()
+    s.friends.append(f)
+    session.commit()
     return json.dumps(s.friends)
+
+
+#username1 is name of guy who initiates friend request
+#username 2 is name of person receiving friend request
+#message is string, which is the message user1 sends to user 2
+@app.route('/addReminder', methods=['POST'])
+def addReminder(username1, username2, message):
+    s1 = session.query(User).get(username1)
+    s2 = session.query(User).get(username2)
+    newReminder = Reminder(id=md5_crypt.encrypt(username1+username2), userTrigger=s1,
+        userReceiver=s2, reminderText=message, time=datetime.utcnow())
+    session.add(newReminder)
+    session.commit()
+    return json.dumps(reminders)
 
 
     
